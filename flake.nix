241dbe968f68d9a47a1b3be40f25251a34c804ba
Simonas Narbutas <simonas@narbuto.lt>
2024-05-28 22:32:12 +0300

    01-intro 00welcome and 01syntax

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "100 days of rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            just
+            (fenix.packages.${system}.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+              "rust-analyzer"
+              "llvm-tools-preview"
+            ])
+            rust-analyzer
+          ];
+          shellHook = ''
+            cat <<EOF
+            Welcome to the '100 days of rust' development shell.
+
+            $(just -l |sed 's/^Available recipes:/The following `just` recipes are available:/')
+            EOF
+            user_shell=$(getent passwd "$(whoami)" |cut -d: -f 7)
+            exec "$user_shell"
+          '';
+        };
+      }
+    );
+}
